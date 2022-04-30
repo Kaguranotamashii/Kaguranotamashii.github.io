61823775bb91471d87f0d1d5bad2a5e26d7c09f4
宋宇然 <63779282+Kaguranotamashii@users.noreply.github.com>
2022-04-30 10:14:38 +0800

    gx

---
+++ js/randombg.js
@@ -0,0 +1,22 @@
+//随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
+var backimg =[
+    "url(https://kaguranotamashii-1308648145.cos.ap-beijing.myqcloud.com/abda03aa7aafb13b4b643516d8c6940a.jpeg)",
+    "url(https://kaguranotamashii-1308648145.cos.ap-beijing.myqcloud.com/20220416112801.png)",
+    "url(https://kaguranotamashii-1308648145.cos.ap-beijing.myqcloud.com/f78266863bfe20d7faf574344b851e80.jpeg)",
+    "url(https://kaguranotamashii-1308648145.cos.ap-beijing.myqcloud.com/6294931695ca322491259a86865d0d76.jpeg)"
+  ];
+  //获取背景图片总数，生成随机数
+  var bgindex =Math.floor(Math.random() * backimg.length);
+  //重设背景图片
+  document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
+  //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
+  var bannerimg =[
+    "url(https://kaguranotamashii-1308648145.cos.ap-beijing.myqcloud.com/abda03aa7aafb13b4b643516d8c6940a.jpeg)",
+    "url(https://kaguranotamashii-1308648145.cos.ap-beijing.myqcloud.com/20220416112801.png)",
+    "url(https://kaguranotamashii-1308648145.cos.ap-beijing.myqcloud.com/f78266863bfe20d7faf574344b851e80.jpeg)",
+    "url(https://kaguranotamashii-1308648145.cos.ap-beijing.myqcloud.com/6294931695ca322491259a86865d0d76.jpeg)"
+  ];
+  //获取banner图片总数，生成随机数
+  var bannerindex =Math.floor(Math.random() * bannerimg.length);
+  //重设banner图片
+  document.getElementById("page-header").style.backgroundImage = bannerimg[bannerindex];
